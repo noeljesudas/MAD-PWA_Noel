ae2ab9f7aab4e0fa7d40420c7b4c4f6f3f5ef972
noeljesudas <141215029+noeljesudas@users.noreply.github.com>
2025-01-27 15:13:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Form Example',
+      title: 'Flutter Form Exahgfhgfhgfmple',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
